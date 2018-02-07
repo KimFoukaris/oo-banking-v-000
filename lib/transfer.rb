79805d3583f0ee1222384c8c1ef1f55f8ae45d87
Kim Foukaris <kimfoukaris@outlook.com>
2018-02-07 12:34:12 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
 
   def execute_transaction
     #if status == "pending" then
-      if self.@sender.valid? == false then
+      if BankAccount@sender.valid? == false then
         #@sender.balance -= @amount
       #  @receiver.balance += @amount
       #  @status = "complete"
