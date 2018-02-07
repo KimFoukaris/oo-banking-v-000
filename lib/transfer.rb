da9c7454274725267e8480ad24c77c6029e34e9d
Kim Foukaris <kimfoukaris@outlook.com>
2018-02-07 12:28:25 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,31 @@
+require 'pry'
+
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+
+  def valid?
+    @sender.valid? && @receiver.valid? ? true : false
+  end
+
+  def execute_transaction
+    if status == "pending" then
+      #if @sender.valid? == true then
+      #  @sender.balance -= @amount
+      #  @receiver.balance += @amount
+      #  @status = "complete"
+    #  else
+        @status = "rejected"
+        message = "Transaction rejected. Please check you account balance."
+        message
+    #  end
+    end
+  end
+
 end
