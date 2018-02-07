953e1cf624f15a6633a2bfe87d08c58bc8fddbd1
Kim Foukaris <kimfoukaris@outlook.com>
2018-02-07 13:11:09 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,7 +29,7 @@ class Transfer
   end
 
   def reverse_transfer
-    binding.pry
+    #binding.pry
     if @status = "complete"
       @sender.balance += @amount
       @receiver.balance -= @amount
